6398c859379897b5b18d3d33e8bc3ee6ea82641f
Eliseo_Nava_Rodriguez <eliseo.nava@cbtis128.edu.mx>
2024-03-19 02:46:49 +0000

    Actualizar

---
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:navadisenio1/screens/dashboard/widgets/item_doctor.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xffb1baec),
+        title: const Text('Dashboard'),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu),
+        ),
+        actions: [
+          const Padding(
+            padding: EdgeInsets.only(right: 15),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://raw.githubusercontent.com/nava128/Img_IOS/main/navaprofile.png"),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.all(15),
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Color(0xffffffff),
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 5),
+                      color:
+                          Theme.of(context).colorScheme.primary.withOpacity(.1),
+                      spreadRadius: 5,
+                      blurRadius: 5)
+                ]),
+            child: const TextField(
+              decoration: InputDecoration(
+                  hintText: "Que quieres ver",
+                  hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                  border: InputBorder.none,
+                  prefixIcon: Icon(Icons.search, color: Colors.brown)),
+            ),
+          ),
+          Container(
+            height: 180,
+            margin: const EdgeInsets.all(15),
+            decoration: const BoxDecoration(
+                // borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                        "https://raw.githubusercontent.com/nava128/Img_IOS/main/doctores.jpg"))),
+          ),
+          ListTile(
+            title: Text("Top doctores"),
+            titleTextStyle: Theme.of(context).textTheme.titleLarge,
+            trailing: Icon(Icons.filter_list_outlined),
+          ),
+          Expanded(
+              child: GridView.count(
+            padding: EdgeInsets.all(15),
+            crossAxisCount: 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 15,
+            children: [for (int i = 1; i <= 10; i++) const Itemdoctor()],
+          ))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/dashboard/widgets/item_doctor.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Itemdoctor extends StatelessWidget {
+  const Itemdoctor({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                offset: Offset(0, 5),
+                color: Theme.of(context).colorScheme.primary.withOpacity(.1),
+                spreadRadius: 5,
+                blurRadius: 5)
+          ]),
+      child: Column(
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://raw.githubusercontent.com/nava128/Img_IOS/main/doctoruno.jpg"),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Nuestros medicos",
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Row(
+            children: List.generate(
+                5,
+                (index) => Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                      size: 18,
+                    )),
+          )
+        ],
+      ),
+    );
+  }
+}
